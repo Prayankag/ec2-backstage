eee61d33fbf71a565184aee3120e1f4c1ae763bb
Prayankag <131856399+Prayankag@users.noreply.github.com>
2024-12-03 11:09:56 +0000

    Update ec2.tf

--- templates/terraform/ec2.tf
+++ templates/terraform/ec2.tf
@@ -10,8 +10,8 @@ terraform {
 provider "aws" {
   region = var.region
   # profile = var.aws_profile
-  access_key = var.access_key
-  secret_key =var.secret_key
+  access_key = var.normala
+  secret_key =var.normals
 }
 
 # Create a VPC
