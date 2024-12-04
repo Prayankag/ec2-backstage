eaacecb702f99b6460d6366c8b153d1aea9321d7
Prayankag <131856399+Prayankag@users.noreply.github.com>
2024-12-04 12:08:44 +0000

    Update backend.tf

--- templates/terraform/backend.tf
+++ templates/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraformbucket43"
-    key            = "terraform.tfstate" 
+    key            = "test/terraform.tfstate" 
     region         = "us-east-1"
   }
 }
